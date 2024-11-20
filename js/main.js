8f9fc3cdc57c3022f44bf83cf8bee46cfc8af14b
ultrasue <nastya.murlla@gmail.com>
2024-11-20 16:30:26 +0300

    add phone mask

---
+++ js/main.js
@@ -0,0 +1,11 @@
+mask("[data-tel-input]");
+
+const phoneInputs = document.querySelectorAll("[data-tel-input");
+phoneInputs.forEach((input) => {
+  input.addEventListener("input", () => {
+    if (input.value == "+") input.value = "";
+  });
+  input.addEventListener("blur", () => {
+    if (input.value == "+") input.value = "";
+  });
+});
