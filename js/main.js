624db116d6b2c94a2dfa0e232024029b9016840b
ultrasue <nastya.murlla@gmail.com>
2024-11-27 11:19:23 +0300

    fix

--- js/main.js
+++ js/main.js
@@ -2,25 +2,25 @@ document.querySelectorAll("[data-tel-input]").forEach(function (input) {
   input.addEventListener("input", function () {
     let value = input.value.replace(/\D/g, "");
     let formattedValue = "";
-    let i = 0;
+    let index = 0;
 
     if (value[0] === "7") {
       formattedValue += "+7";
       maxLength = 22;
-      i = 1;
+      index = 1;
     } else if (value[0] === "8") {
       formattedValue += "8";
       maxLength = 21;
-      i = 1;
+      index = 1;
     } else {
       input.value = formattedValue;
       return;
     }
 
-    while (i < value.length && formattedValue.length < maxLength) {
-      if (i === 1) formattedValue += " (";
-      else if (i === 4) formattedValue += ") ";
-      else if (i === 7 || i === 9) formattedValue += " - ";
+    while (index < value.length && formattedValue.length < maxLength) {
+      if (index === 1) formattedValue += " (";
+      else if (index === 4) formattedValue += ") ";
+      else if (index === 7 || index === 9) formattedValue += " - ";
       formattedValue += value[i];
       i++;
     }
